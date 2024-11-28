ca8b81985c62ff4f446ac5e9884c8cd56dee904d
HarlemLineHustle <contact@harlemlinehustle.com>
2024-11-27 22:35:41 -0500

    Commenting out server files while I work on them

--- server/engine/action.ts
+++ server/engine/action.ts
@@ -1,13 +1,15 @@
-import { Character } from './character/character';
+// import { Character } from './character/character';
 
-type ActionArgs = {
-  actor: Character;
-  cooldown: number;
-  cost: number;
-  description: string;
-  name: string;
-  onPerform: () => void;
-  range: number;
-  target: Character;
-};
-export const action = ({ actor }) => {};
+// type ActionArgs = {
+//   actor: Character;
+//   cooldown: number;
+//   cost: number;
+//   description: string;
+//   name: string;
+//   onPerform: () => void;
+//   range: number;
+//   target: Character;
+// };
+// export const action = ({ actor }) => {
+
+// };
